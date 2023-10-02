1732256f5b0b1913aee6d80f2381d49f21f77e22
hunterlan <konstantin.sharykin@outlook.com>
2023-10-03 00:09:37 +0200

    Add primary and unique keys

--- dotnet/DocSession.Application/Helpers/ApplicationContext.cs
+++ dotnet/DocSession.Application/Helpers/ApplicationContext.cs
@@ -10,4 +10,19 @@ public class ApplicationContext : DbContext
 
   public DbSet<User> Users => Set<User>();
   public DbSet<Appointment> Appointments => Set<Appointment>();
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<User>()
+      .HasKey(u => u.Id)
+      .HasName("PrimaryKey_UserId");
+
+    modelBuilder.Entity<User>()
+      .HasIndex(u => new { u.Username, u.Email })
+      .IsUnique();
+
+    modelBuilder.Entity<Appointment>()
+      .HasKey(a => a.Id)
+      .HasName("PrimaryKey_AppointmentId");
+  }
 }
